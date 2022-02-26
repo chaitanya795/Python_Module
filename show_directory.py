29450fe9a2f050fb2851b81cc8269473b8999298
ram-1604 <45118457+ram-1604@users.noreply.github.com>
2022-02-26 20:00:45 +0530

    Add files via upload

---
+++ show_directory.py
@@ -0,0 +1,5 @@
+
+#importing the module to check the directory list
+
+import os 
+print(os.listdir())
---
+++ text_to_speech.py
@@ -0,0 +1,4 @@
+import pyttsx3
+engine = pyttsx3.init()
+engine.say("Hey Rama Im a python Module and wellcome to world of python")
+engine.runAndWait()
