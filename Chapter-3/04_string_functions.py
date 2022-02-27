42d2757f28a18bbb3f66358e3852bedc9bbf4a8f
ram-1604 <45118457+ram-1604@users.noreply.github.com>
2022-02-27 12:44:31 +0530

    Update 04_string_functions.py

--- Chapter-3/04_string_functions.py
+++ Chapter-3/04_string_functions.py
@@ -5,7 +5,7 @@ print(myStr.endswith("xyz"))
 print(myStr.startswith("abcd"))
 print(myStr.count('ab'))
 
-myStr = "my name is harry name"
+myStr = "my name is Ram Narayana Reddy"
 print(myStr.capitalize())
 print(myStr.find("name"))
 print(myStr.replace("name", "date"))
