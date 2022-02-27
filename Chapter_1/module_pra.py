06ac97a20b911a5d0d81e462a2053b82398ac5fd
ram-1604 <45118457+ram-1604@users.noreply.github.com>
2022-02-27 09:00:10 +0530

    Rename module_pra.py to Chapter_1/module_pra.py

---
+++ Chapter_1/module_pra.py
@@ -0,0 +1,4 @@
+import time
+import flask
+time.sleep(3)
+print("wait for 3 sec to run this program")
--- module_pra.py
+++
@@ -1,4 +0,0 @@
-import time
-import flask
-time.sleep(3)
-print("wait for 3 sec to run this program")
